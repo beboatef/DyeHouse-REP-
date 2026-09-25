567ba28920a6d84e21993ad12420edbf95108ccf
beboatef <abanoub.atef.lam3y@gmail.com>
2026-09-25 19:41:58 +0000

    Protect customer account number default

--- DyeHouseERP/src/DyeHouseERP.Persistence/Configurations/CustomerConfiguration.cs
+++ DyeHouseERP/src/DyeHouseERP.Persistence/Configurations/CustomerConfiguration.cs
@@ -13,7 +13,7 @@ public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
 
         builder.Property(c => c.Code).HasMaxLength(30).IsRequired();
         builder.Property(c => c.Name).HasMaxLength(200).IsRequired();
-        builder.Property(c => c.AccountNumber).HasMaxLength(50).IsRequired();
+        builder.Property(c => c.AccountNumber).HasMaxLength(50).IsRequired().HasDefaultValueSql("CONVERT(nvarchar(50), [Code])");
         builder.HasIndex(c => c.AccountNumber).IsUnique();
         builder.Property(c => c.CreatedBy).HasMaxLength(100).IsRequired();
         builder.Property(c => c.ModifiedBy).HasMaxLength(100);
